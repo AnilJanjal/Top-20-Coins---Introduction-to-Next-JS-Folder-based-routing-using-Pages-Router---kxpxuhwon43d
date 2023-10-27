eedc9503da777007e28d6e7d96847eb8c5fc4a8c
Anil Devidas Janjal <132841194+AnilJanjal@users.noreply.github.com>
2023-10-27 19:46:14 +0530

    Update CoinDisplay.js
    changes

--- components/CoinDisplay.js
+++ components/CoinDisplay.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from "react";
 import CoinCard from "./CoinCard";
+import axios from "axios";
 
-function CoinDisplay({ coin }) {
+function CoinDisplay() {
   const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.coinlore.net/api/tickers/"
+        );
+        setCoins(response.data.data.slice(0, 20));
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="home">
       <h1>Top 20 Cryptos</h1>
       <div className="coins-container">
         {coins.map((coin) => (
-          <CoinCard />
+          <CoinCard key={coin.id} coin={coin} />
         ))}
       </div>
     </div>
@@ -17,3 +32,4 @@ function CoinDisplay({ coin }) {
 }
 
 export default CoinDisplay;
+
